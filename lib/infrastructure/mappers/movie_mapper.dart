8d8149b4cc58e5f211e893c93a67dbb93e675a3d
acensito <acenforos@gmail.com>
2025-07-20 21:07:47 +0200

    Implementacion repositorio

---
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -0,0 +1,27 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
+
+class MovieMapper {
+  static Movie movieDBToEntity(MovieMovieDB movieDB) {
+    return Movie(
+      adult: movieDB.adult,
+      backdropPath: (movieDB.backdropPath.isNotEmpty) 
+      ? 'https://image.tmdb.org/t/p/w500${movieDB.backdropPath}'
+      : 'https://media.istockphoto.com/id/1409329028/vector/no-picture-available-placeholder-thumbnail-icon-illustration-design.jpg?s=612x612&w=0&k=20&c=_zOuJu755g2eEUioiOUdz_mHKJQJn-tDgIAhQzyeKUQ=',
+      genreIds: movieDB.genreIds.map((id) => id.toString()).toList(),
+      id: movieDB.id,
+      originalLanguage: movieDB.originalLanguage,
+      originalTitle: movieDB.originalTitle,
+      overview: movieDB.overview,
+      popularity: movieDB.popularity,
+      posterPath: (movieDB.posterPath.isNotEmpty) 
+      ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
+      : 'no-poster',
+      releaseDate: movieDB.releaseDate,
+      title: movieDB.title,
+      video: movieDB.video,
+      voteAverage: movieDB.voteAverage,
+      voteCount: movieDB.voteCount,
+    );
+  }
+}
---
+++ lib/infrastructure/repository/movie_repository_impl.dart
@@ -0,0 +1,16 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/domain/repositories/movies_repository.dart';
+import 'package:cinemapedia/infrastructure/datasources/moviedb_datasource.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+
+  final MovieDBDatasource datasource;
+
+  MovieRepositoryImpl({required this.datasource});
+
+  @override
+  Future<List<Movie>> getNowPlaying({int page = 1}) {
+    return datasource.getNowPlaying(page: page);
+  }
+
+}
