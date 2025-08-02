87143bab0c5163cf373e7667b99d363321945a04
acensito <acenforos@gmail.com>
2025-08-02 23:05:44 +0200

    Movie Screen: SliverAppBar

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -42,12 +42,73 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     }
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(movie.title),
+      body: CustomScrollView(
+        physics: const ClampingScrollPhysics(),
+        slivers: [
+          _CustomSliverAppBar(movie: movie),
+        ],
       ),
-      body: Center(
-        child: Text('Details for movie ID: ${widget.movieId}'),
+    );
+  }
+}
+
+class _CustomSliverAppBar extends StatelessWidget {
+
+  final Movie movie;
+
+  const _CustomSliverAppBar({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+    return SliverAppBar(
+      backgroundColor: Colors.black,
+      expandedHeight: size.height * 0.7,
+      foregroundColor: Colors.white,
+      flexibleSpace: FlexibleSpaceBar(
+        titlePadding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+        title: Text(
+          movie.title,
+          style: const TextStyle(fontSize: 20, color: Colors.white),
+          textAlign: TextAlign.start,
+        ),
+        background: Stack(children: [
+          SizedBox.expand(
+            child: Image(image: NetworkImage(movie.posterPath), fit: BoxFit.cover),
+          ),
+          const SizedBox.expand(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: [0.7, 1.0],
+                  colors: [
+                    Colors.transparent,
+                    Colors.black87,
+                  ],
+                ),
+              ),
+            ),
+          ),
+          const SizedBox.expand(
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  stops: [0.0, 0.3],
+                  colors: [
+                    Colors.black87,
+                    Colors.transparent,
+                  ],
+                ),
+              ),
+            ),
+          )
+        ]),
       )
+
     );
   }
 }
