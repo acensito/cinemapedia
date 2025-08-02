e22dd6623da01f3200c4396e1abb279e25e2b201
acensito <acenforos@gmail.com>
2025-08-02 18:25:44 +0200

    Movie Screen

---
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MovieScreen extends StatelessWidget {
+
+  static const String routeName = 'movie-screen';
+
+  final String movieId;
+
+  const MovieScreen({super.key, required this.movieId});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
