933b83c3abffca64d2114801a3c13bd71b7f66f2
acensito <acenforos@gmail.com>
2025-08-03 20:15:07 +0200

    Show movie suggestions

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -17,7 +17,7 @@ class MovieMapper {
       popularity: movieDB.popularity,
       posterPath: (movieDB.posterPath.isNotEmpty)
           ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
-          : 'no-poster',
+          : 'https://movienewsletters.net/photos/000000H1.jpg',
       releaseDate: movieDB.releaseDate,
       title: movieDB.title,
       video: movieDB.video,
@@ -40,7 +40,7 @@ class MovieMapper {
       popularity: movieDB.popularity,
       posterPath: (movieDB.posterPath.isNotEmpty)
           ? 'https://image.tmdb.org/t/p/w500${movieDB.posterPath}'
-          : 'no-poster',
+          : 'https://movienewsletters.net/photos/000000H1.jpg',
       releaseDate: movieDB.releaseDate,
       title: movieDB.title,
       video: movieDB.video,
--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -5,7 +5,6 @@ import 'package:flutter/material.dart';
 typedef SearchMoviesCallBack = Future<List<Movie>> Function(String query);
 
 class SearchMovieDelegate extends SearchDelegate<Movie?> {
-
   final SearchMoviesCallBack searchMovies;
 
   SearchMovieDelegate({required this.searchMovies});
@@ -17,11 +16,11 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
   List<Widget>? buildActions(BuildContext context) {
     //acciones al buscar en el boton derecho
     return [
-        FadeIn(
-          animate: query.isNotEmpty,
-          duration: Duration(milliseconds: 100),
-          child: IconButton(onPressed: () => query = '', 
-          icon: Icon(Icons.clear)))
+      FadeIn(
+        animate: query.isNotEmpty,
+        duration: Duration(milliseconds: 100),
+        child: IconButton(onPressed: () => query = '', icon: Icon(Icons.clear)),
+      ),
     ];
   }
 
@@ -29,7 +28,10 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
   Widget? buildLeading(BuildContext context) {
     //el icono de regreso y que debe de hacer al ser pulsado
     //no retorna valor ninguno (null)
-    return IconButton(onPressed: () => close(context, null), icon: Icon(Icons.arrow_back));
+    return IconButton(
+      onPressed: () => close(context, null),
+      icon: Icon(Icons.arrow_back),
+    );
   }
 
   @override
@@ -46,13 +48,55 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
         return ListView.builder(
           itemCount: movies?.length ?? 0,
-        itemBuilder: (context, index) {
-          final movie = movies?[index];
-          return ListTile(
-            title: Text(movie?.title ?? ''),
-          );
-        });
-    });
+          itemBuilder: (context, index) {
+            final movie = movies?[index];
+            if (movie == null) return const SizedBox.shrink();
+            return _MovieItem(movie);
+          },
+        );
+      },
+    );
   }
+}
 
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem(this.movie);
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        children: [
+          SizedBox(
+            width: size.width * 0.2,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                movie.posterPath,
+                loadingBuilder: (context, child, loadingProgress) =>
+                    FadeIn(child: child),
+              ),
+            ),
+          ), //Sizedbox portada
+          SizedBox(width: 10),
+          SizedBox(
+            width: size.width*0.7,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+              Text(movie.title, style: textStyles.titleMedium),
+              (movie.overview.length > 100)
+              ? Text('${movie.overview.substring(0,100)}...')
+              : Text(movie.overview)
+            ]))
+        ],
+      ),
+    );
+  }
 }
