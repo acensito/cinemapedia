2384d38e55c9fe5438a2ce9d0b0308ed6cd92b10
acensito <acenforos@gmail.com>
2025-08-03 17:31:43 +0200

    buildLeading y buildActions

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,18 +1,29 @@
+import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
-class SearchMovieDelegate extends SearchDelegate {
+class SearchMovieDelegate extends SearchDelegate<Movie?> {
 
   @override
   String get searchFieldLabel => 'Buscar película';
 
   @override
   List<Widget>? buildActions(BuildContext context) {
-    return [const Text('buildActions')];
+    //acciones al buscar
+    return [
+        FadeIn(
+          animate: query.isNotEmpty,
+          duration: Duration(milliseconds: 100),
+          child: IconButton(onPressed: () => query = '', 
+          icon: Icon(Icons.clear)))
+    ];
   }
 
   @override
   Widget? buildLeading(BuildContext context) {
-    return const Text('buildLeading');
+    //el icono de regreso y que debe de hacer al ser pulsado
+    //no retorna valor ninguno (null)
+    return IconButton(onPressed: () => close(context, null), icon: Icon(Icons.arrow_back));
   }
 
   @override
