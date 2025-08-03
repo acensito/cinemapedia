271af8c60d3eeaae39065ffa998cbcadd8d2d0f6
acensito <acenforos@gmail.com>
2025-08-03 13:14:10 +0200

    get actors by movie prueba

--- lib/presentation/screens/movies/movie_screen.dart
+++ lib/presentation/screens/movies/movie_screen.dart
@@ -1,5 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
-import 'package:cinemapedia/presentation/providers/movies/movie_detail_provider.dart';
+import 'package:cinemapedia/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -22,6 +22,7 @@ class MovieScreenState extends ConsumerState<MovieScreen> {
     // hacemos la peticion http del provider
     // para obtener los detalles de la pelicula
     ref.read(movieDetailProvider.notifier).loadMovie(widget.movieId);
+    ref.read(actorsByMovieProvider.notifier).loadActors(widget.movieId);
   }
 
   @override
