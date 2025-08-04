117075cf241ec47fc6bba6e9fefdd4f3da861afd
acensito <acenforos@gmail.com>
2025-08-04 19:11:58 +0200

    calificacion pelicula busqueda

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,4 +1,5 @@
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_format.dart';
 import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:flutter/material.dart';
 
@@ -93,8 +94,16 @@ class _MovieItem extends StatelessWidget {
               Text(movie.title, style: textStyles.titleMedium),
               (movie.overview.length > 100)
               ? Text('${movie.overview.substring(0,100)}...')
-              : Text(movie.overview)
-            ]))
+              : Text(movie.overview),
+              Row(
+                children: [
+                  Icon(Icons.star_half_rounded, color: Colors.yellow.shade800),
+                  SizedBox(width: 5),
+                  Text(HumanFormat.number(movie.voteAverage), style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900),)
+                ],
+              )
+            ])),
+
         ],
       ),
     );
