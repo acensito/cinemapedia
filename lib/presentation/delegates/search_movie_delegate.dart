5d5b1d241703cc7fad90c1cf315c988ea88813fe
acensito <acenforos@gmail.com>
2025-08-04 19:37:49 +0200

    busqueda con devolucion de argumentos

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -52,7 +52,7 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
           itemBuilder: (context, index) {
             final movie = movies?[index];
             if (movie == null) return const SizedBox.shrink();
-            return _MovieItem(movie);
+            return _MovieItem(movie, close);
           },
         );
       },
@@ -61,50 +61,57 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
 }
 
 class _MovieItem extends StatelessWidget {
-  final Movie movie;
 
-  const _MovieItem(this.movie);
+  final Movie movie;
+  final Function onMovieSelected;
+
+  const _MovieItem(this.movie, this.onMovieSelected);
 
   @override
   Widget build(BuildContext context) {
     final textStyles = Theme.of(context).textTheme;
     final size = MediaQuery.of(context).size;
 
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-      child: Row(
-        children: [
-          SizedBox(
-            width: size.width * 0.2,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.network(
-                movie.posterPath,
-                loadingBuilder: (context, child, loadingProgress) =>
-                    FadeIn(child: child),
+    return GestureDetector(
+      onTap: () {
+        onMovieSelected(context, movie);
+      },
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+        child: Row(
+          children: [
+            SizedBox(
+              width: size.width * 0.2,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.network(
+                  movie.posterPath,
+                  loadingBuilder: (context, child, loadingProgress) =>
+                      FadeIn(child: child),
+                ),
               ),
-            ),
-          ), //Sizedbox portada
-          SizedBox(width: 10),
-          SizedBox(
-            width: size.width*0.7,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-              Text(movie.title, style: textStyles.titleMedium),
-              (movie.overview.length > 100)
-              ? Text('${movie.overview.substring(0,100)}...')
-              : Text(movie.overview),
-              Row(
+            ), //Sizedbox portada
+            SizedBox(width: 10),
+            SizedBox(
+              width: size.width*0.7,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(Icons.star_half_rounded, color: Colors.yellow.shade800),
-                  SizedBox(width: 5),
-                  Text(HumanFormat.number(movie.voteAverage), style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900),)
-                ],
-              )
-            ])),
-
-        ],
+                Text(movie.title, style: textStyles.titleMedium),
+                (movie.overview.length > 100)
+                ? Text('${movie.overview.substring(0,100)}...')
+                : Text(movie.overview),
+                Row(
+                  children: [
+                    Icon(Icons.star_half_rounded, color: Colors.yellow.shade800),
+                    SizedBox(width: 5),
+                    Text(HumanFormat.number(movie.voteAverage), style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900),)
+                  ],
+                )
+              ])),
+      
+          ],
+        ),
       ),
     );
   }
--- lib/presentation/widgets/share/custom_appbar.dart
+++ lib/presentation/widgets/share/custom_appbar.dart
@@ -1,7 +1,9 @@
+import 'package:cinemapedia/domain/entities/movie.dart';
 import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 import 'package:cinemapedia/presentation/providers/movies/movies_repository_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
 
 class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
@@ -25,17 +27,25 @@ class CustomAppbar extends ConsumerWidget {
               Text('Cinemapedia', style: titleStyle?.copyWith(color: colors.primary)),
               const Spacer(),
       
-              IconButton(onPressed: () {
+              IconButton(
+                onPressed: () {
 
-                final movieRepository = ref.read(movieRepositoryProvider);
+                  final movieRepository = ref.read(movieRepositoryProvider);
 
-                showSearch(
-                  context: context,
-                  delegate: SearchMovieDelegate(
-                    searchMovies: movieRepository.searchMovies
-                  ));
-              }, 
-              icon: Icon(Icons.search))
+                  final navigator = GoRouter.of(context);
+                  //la busqueda devuelve una pelicula si existiera
+                  showSearch<Movie?>(
+                    context: context,
+                    delegate: SearchMovieDelegate(searchMovies: movieRepository.searchMovies)
+                    //si existe, navega hasta la pelicula concreta
+                  ).then((movie) {
+                    if (movie == null) return;
+                    navigator.push('/movie/${movie.id}');
+                  });
+
+                }, 
+                icon: Icon(Icons.search)
+              )
             ],
           ),
         ),
